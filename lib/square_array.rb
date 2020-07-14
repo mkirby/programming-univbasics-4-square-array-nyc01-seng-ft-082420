b2b28c7c27c5d153163019a92a7ae0e3dfa54a0e
Matthew Kirby <matthew.robbins.kirby@gmail.com>
2020-07-14 19:15:02 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
+  squaredArray = []
+  #counter = 0
+  #while counter < array.length do
+    #squaredArray.push(array[counter] ** 2)
+    #counter += 1
+  #end
+  
+  array.length.times do |index|
+    squaredArray.push(array[index] ** 2)
+  end
+  squaredArray
 end
